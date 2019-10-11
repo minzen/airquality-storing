b93ae81c0a51825949b39ba1d3ebaa7da5cbf61a
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2019-10-11 15:31:20 +0200

    Updates some libraries, takes dotenv into use

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import App from './app'
 import MeasurementController from './controllers/measurementcontroller'
+const dotenv = require('dotenv')
+dotenv.config()
+const port: number = parseInt(`${process.env.PORT}`, 10) || 3000
 
-const app = new App([new MeasurementController()], 3000)
+const app = new App([new MeasurementController()], port)
 app.listen()
