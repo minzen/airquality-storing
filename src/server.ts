bdefeff5ff2795224ef12a8819a7e46aac776518
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2020-02-24 23:24:24 +0100

    Start from the beginning with the basic express skeleton to be used for providing an API

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import App from './app'
-import MeasurementController from './controllers/measurementcontroller'
-import AuthenticationController from './controllers/authenticationcontroller'
 const dotenv = require('dotenv')
 dotenv.config()
 const port: number = parseInt(`${process.env.PORT}`, 10) || 3000
+import * as express from 'express'
+const app = express()
 
-const app = new App(
-  [new MeasurementController(), new AuthenticationController()],
-  port,
-)
-app.listen()
+app.get('/measurements', (req: express.Request, res: express.Response) => {
+  res.send('Get measurements')
+})
+
+app.post('/measurement', (req: express.Request, res: express.Response) => {
+  res.send('Post response')
+})
+
+app.listen(port)
+console.log('start server at port', port)
